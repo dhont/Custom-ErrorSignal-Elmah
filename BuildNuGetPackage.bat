a40b4b5b57a57867a986a2b0a1cd4ab381bca359
Dragos Hont <dragos79@gmail.com>
2015-10-15 11:38:05 +0300

    v 1.0.9: Removes an erroneous nuget build configuration that excludes the ElmahExtenions.dll

--- BuildNuGetPackage.bat
+++ BuildNuGetPackage.bat
@@ -9,5 +9,5 @@ Echo Rebuilding Solution
 %MSBuildPath%\MSBuild.exe %SolutionOrProjectPath% /p:VisualStudioVersion=12.0 /t:rebuild
 
 Echo Creating Package
-%NugetPath%\NuGet.exe pack %NugetCsproj% -Prop Configuration=Release -Exclude **\*.dll -Exclude web.config -Exclude Web.config.transform -Exclude Global.asax -Exclude Scripts/*
+%NugetPath%\NuGet.exe pack %NugetCsproj% -Prop Configuration=Release 
 pause
